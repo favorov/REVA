01fafaaa731b9257681eb5acaca1793137be4172
Alexander Favorov <favorov@sensi.org>
2020-11-05 02:53:44 -0500

    v 0.0.3

---
+++ R/R/REVA.R
@@ -0,0 +1,17 @@
+#' REVA: testing whther a sclalar value differs less for coler points  
+#'
+#' We hava a scalra value for a set of points and we have a distance between the points.
+#' Null ypothesis: the scalar does not depend ont the location, so the values differ between distant points as strons as between close points
+#' Main function: 
+#' 
+#' @section REVA functions:
+#'	REVA.p.value calculates the p-value corresponding to the null
+#'	REVA.user.distance calculate distance as Kendall tau and calculate the p-value	
+#'	REVA.user.distance.Geneset does something
+
+#' @docType package
+#' @name REVA 
+#' @useDynLib REVA
+#' @importFrom Rcpp evalCpp
+# these two are Rcpp - scecific ivocations 
+NULL
