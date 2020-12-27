a99ec86865fa9f6b0d290d925cdc17d6af95a0d2
Alexander Favorov <favorov@sensi.org>
2020-12-27 05:34:36 -0500

    packge documantation fixed a bit

--- R/R/REVA.R
+++ R/R/REVA.R
@@ -1,13 +1,13 @@
 #' REVA: testing whther a sclalar value differs less for coler points  
 #'
-#' We hava a scalra value for a set of points and we have a distance between the points.
-#' Null ypothesis: the scalar does not depend ont the location, so the values differ between distant points as strons as between close points
+#' We hava a scalar value for a set of points and we have a distance between the points.
+#' Null hypothesis: the scalar does not depend ont the location, so the values differ between distant points as strons as between close points
 #' Main function: 
 #' 
 #' @section REVA functions:
 #'	REVA.test calculates the p-value corresponding to the null
 #'	REVA.user.distance calculate distance as Kendall tau and calculate the p-value	
-#'	REVA.user.distance.Geneset does something
+#'	REVA.user.distance.Geneset does REVA.user.distance for a gene set
 
 #' @docType package
 #' @name REVA 
